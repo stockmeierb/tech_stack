1b06319e6332215edeea08dec219618b1d915158
Brett E Stockmeier <stockmeierb@gmail.com>
2019-08-03 23:21:29 -0500

    create library list display component and hook up data from reducer

---
+++ src/components/LibaryList.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+class LibaryList extends Component {
+  render() {
+    return;
+  }
+}
+
+export default connect()(LibaryList);
